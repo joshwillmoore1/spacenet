b7c491c67f5ccf2cedbcb195607be2e942064606
Joshua Moore <45179849+joshwillmoore1@users.noreply.github.com>
2026-01-29 17:09:40 +0000

    tests

---
+++ tests/test_core_functions.py
@@ -0,0 +1,44 @@
+import sys
+sys.path.append('..')
+
+import pytest
+import numpy as np
+import netpcf as npc
+
+@pytest.fixture
+def setup_network_and_params():
+    
+    # define a spatial region and generate a Poisson point process
+    region_size = (1000, 1000)
+    lambda_points = 0.001  
+    num_points = int(region_size[0] * region_size[1] * lambda_points)
+    points = np.random.uniform(0, region_size[0], size=(num_points, 2))
+    
+
+    netPCF_kwargs = dict(spatial_kernel_bandwidth=75,
+                        spatial_kernel_n=2,
+                        r_min=0, 
+                        r_max=700,
+                        r_step=10, 
+                        return_confidence_interval=True,
+                        verbose=False,
+                        n_jobs=1)  
+
+    G_ppp=npc.helpers.generate_spatial_network(points,network_type='delaunay',max_edge_distance=100)
+    
+    return G_ppp, netPCF_kwargs
+
+
+def test_null_cross_pcf(setup_network_and_params):
+    this_metwork,netPCF_kwargs = setup_network_and_params
+    
+    
+    # compute the cross-PCF
+    r,g,CI = npc.cross_pair_correlation_function(this_metwork,**netPCF_kwargs)
+    
+    
+    # check that g is approximately 1 for all r values
+    assert np.allclose(g, 1, atol=0.2), "Cross-PCF deviates significantly from 1 for a homogeneous Poisson process."
+
+    
+    
